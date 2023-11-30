38b3931b31796a6e8aa5396d5304f1f6458b5f32
mamasabirov kushtarbek <kushtarmamasabirov@gmail.com>
2023-11-30 16:50:49 +0600

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import Menu from "./components/menu/Menu";
 
 const App = () => {
   return (
     <div>
-      App
+      <Menu />
     </div>
   );
 };
