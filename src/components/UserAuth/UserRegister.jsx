748534d5a9470d7eef705421df0a00af75f9b716
Abai <abaiesen04@gmail.com>
2023-12-01 15:48:40 +0600

    transition between 2 auth modals

--- src/components/UserAuth/UserRegister.jsx
+++ src/components/UserAuth/UserRegister.jsx
@@ -2,20 +2,22 @@ import { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { Link } from 'react-router-dom';
 
-function UserRegister() {
-  const [show, setShow] = useState(false);
+function UserRegister(props) {
+  const {showReg, setShowReg, setShowLogin} = props
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleClose = () => setShowReg(false);
+
+  const handleTransition = () => {
+    handleClose()
+    setShowLogin(true)
+  }
+
 
   return (
     <>
-      <Button variant='light' style={{width: "100%", textAlign: "start"}} onClick={handleShow}>
-        Регистрация
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={showReg} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Регистрация</Modal.Title>
         </Modal.Header>
@@ -23,31 +25,26 @@ function UserRegister() {
           <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Эл-почта</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email"  />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Пароль</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password"  />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Подтвердите пароль</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password"  />
       </Form.Group>
       
       <Button variant="primary" type="submit">
-        Submit
+        Отправить
       </Button>
     </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
+          <Button onClick={handleTransition} variant='outline-primary'>У меня есть аккаунт</Button>
         </Modal.Footer>
       </Modal>
     </>
