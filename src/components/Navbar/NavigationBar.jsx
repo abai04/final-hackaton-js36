da6600d4e418487364a39065d1cddabad1f449eb
Abai <abaiesen04@gmail.com>
2023-12-04 10:36:59 +0600

    .

--- src/components/Navbar/NavigationBar.jsx
+++ src/components/Navbar/NavigationBar.jsx
@@ -5,11 +5,10 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import UserLogin from '../UserAuth/UserLogin';
 import UserRegister from '../UserAuth/UserRegister';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function NavigationBar() {
-  const [showLogin, setShowLogin] = useState("false")
-  const [showReg, setShowReg] = useState("false")
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -18,14 +17,9 @@ function NavigationBar() {
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav.Link href="#home">Еда</Nav.Link>
             <Nav.Link style={{marginLeft: "20px"}} href="#link">Курьерская служба</Nav.Link>
-            <NavDropdown style={{marginLeft: "auto"}} title="Авторизация" id="basic-nav-dropdown">
-
-              <Button onClick={() => setShowLogin("true")} variant='light'>Войти</Button>
-              <UserLogin showReg={showReg} setShowReg={setShowReg} showLogin={showLogin} setShowLogin={setShowLogin}/>
-
-              <Button onClick={()=> setShowReg("true")} variant='light'>Регистрация</Button>
-              <UserRegister  showReg={showReg} setShowReg={setShowReg} showLogin={showLogin} setShowLogin={setShowLogin}/>
-
+            <NavDropdown className='ms-auto' title="Авторизация">
+              <UserLogin/>
+              <UserRegister/>
             </NavDropdown>
         </Navbar.Collapse>
       </Container>
